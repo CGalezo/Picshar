775ab9f5504d82a5af099455bc78ebd4f4f646b8
Jose Maza <jgmaza@uninorte.edu.co>
2022-05-26 17:19:15 -0500

    users/login + jwt

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,5 +1,19 @@
-const express = require('express');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 //const controller = require('../controllers/user.controller');
+
+router.get("/login", (req, res) => {
+  res.json({ mensaje: "LoginPage" });
+});
+
+router.post("/login", (req, res) => {
+  const user = req.body;
+  jwt.sign({ user }, "secretkey", (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const jwt = require("jsonwebtoken")
+
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
 
 // Routes
-const exampleUrls = require('./routes/route') // Import the example routes, delete this when first issue resolved 
+const users = require('./routes/users.routes') // Import the example routes, delete this when first issue resolved 
 
 dotenv.config()
 
@@ -14,6 +16,6 @@ mongoose.connect(process.env.DATABASE_ACCESS, ()=>console.log("Database connecte
 app.use(express.json())
 app.use(cors())
 
-app.use('/app', exampleUrls)
+app.use('/users', users)
 
 app.listen(4000, () => console.log("Server is up and running"))
