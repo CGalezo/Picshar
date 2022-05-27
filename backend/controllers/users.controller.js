74e927c1d9d79f7ecab767d40e57906ab10ddd49
Guacha <guachamalinga@gmail.com>
2022-05-27 15:00:08 -0500

    implemented controller and route for get w/ query

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -42,4 +42,34 @@ const registerUser = async (req, res) => {
   });
 };
 
-module.exports = { registerUser };
+const getUser = async (req, res) => {
+  const { id } = req.query;
+  if (!id) {
+    return res.status(400).json({
+      message: 'Please provide an id',
+    });
+  } else {
+    User.findById(id, (err, user) => {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error getting user',
+        });
+      }
+      if (!user) {
+        return res.status(404).json({
+          message: 'User not found',
+        });
+      }
+      return res.status(200).json({
+        message: 'User found',
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        birthdate: user.birthdate,
+        bio: user.bio,
+      });
+    });
+  }
+};
+
+module.exports = { registerUser, getUser };
