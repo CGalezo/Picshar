21cff2d698b3643fff6ea56e5dcb7478de458c38
Guacha <guachamalinga@gmail.com>
2022-05-31 11:21:34 -0500

    Feat: Get posts saved by user given their id

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -61,7 +61,8 @@ const getPostsLikedByUser = async (req, res) => {
       message: 'User not found',
     });
   }
-  if (user.public_likes || user._id === requester_id) {
+  console.log(user._id, requester_id);
+  if (user.public_likes || author === requester_id) {
     Post.find({ _id: { $in: user.liked_posts } }, (err, posts) => {
       if (err) {
         return res.status(500).json({
@@ -85,4 +86,47 @@ const getPostsLikedByUser = async (req, res) => {
   }
 };
 
-module.exports = { getPostsByUser, getPostsLikedByUser };
+const getPostsSavedByUser = async (req, res) => {
+  const token = req.headers['x-access-token'] || req.query.token || req.body.token;
+  const user_id = req.query.user_id || req.query.author;
+  if (!token) {
+    return res.status(401).json({
+      message: 'Please provide a token',
+    });
+  }
+  const requester_id = Tokenizer.userIdFromToken(token);
+  if (!requester_id) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+  if (user_id !== requester_id) {
+    return res.status(401).json({
+      message: "You are not authorized to view this user's saved posts",
+    });
+  }
+  const user = await User.findById(user_id);
+  if (!user) {
+    return res.status(404).json({
+      message: 'User not found',
+    });
+  }
+  Post.find({ _id: { $in: user.saved_posts } }, (err, posts) => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Error getting posts',
+      });
+    }
+    if (!posts) {
+      return res.status(404).json({
+        message: 'No posts found',
+      });
+    }
+    return res.status(200).json({
+      message: 'Saved Posts retrieved',
+      posts,
+    });
+  });
+};
+
+module.exports = { getPostsByUser, getPostsLikedByUser, getPostsSavedByUser };
