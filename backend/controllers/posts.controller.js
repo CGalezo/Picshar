3c0d21c1425a4cb4846e2e5422d48c8c2fe776eb
Guacha <guachamalinga@gmail.com>
2022-05-31 11:07:14 -0500

    Feat: Get Liked posts of a user given their id

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -41,4 +41,48 @@ const getPostsByUser = async (req, res) => {
   });
 };
 
-module.exports = { getPostsByUser };
+const getPostsLikedByUser = async (req, res) => {
+  const token = req.headers['x-access-token'] || req.query.token || req.body.token;
+  const { author } = req.query;
+  if (!token) {
+    return res.status(401).json({
+      message: 'Please provide a token',
+    });
+  }
+  const requester_id = Tokenizer.userIdFromToken(token);
+  if (!requester_id) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+  const user = await User.findById(author);
+  if (!user) {
+    return res.status(404).json({
+      message: 'User not found',
+    });
+  }
+  if (user.public_likes || user._id === requester_id) {
+    Post.find({ _id: { $in: user.liked_posts } }, (err, posts) => {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error getting posts',
+        });
+      }
+      if (!posts) {
+        return res.status(404).json({
+          message: 'No posts found',
+        });
+      }
+      return res.status(200).json({
+        message: 'Liked Posts retrieved',
+        posts,
+      });
+    });
+  } else {
+    return res.status(401).json({
+      message: "You are not authorized to view this user's liked posts",
+    });
+  }
+};
+
+module.exports = { getPostsByUser, getPostsLikedByUser };
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/posts.controller');
 
 router.get('/', controller.getPostsByUser);
+router.get('/liked-by', controller.getPostsLikedByUser);
 
 module.exports = router;
