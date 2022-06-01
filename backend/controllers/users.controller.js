aeb5adceeed1d5c819b350b91265b522cb54cdf3
Guacha <guachamalinga@gmail.com>
2022-06-01 13:27:15 -0500

    Feat: Added routes for follows / followers

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -121,4 +121,123 @@ const loginUser = async (req, res) => {
   });
 };
 
-module.exports = { registerUser, getUser, loginUser };
+const getFollowers = async (req, res) => {
+  const token = req.headers['x-access-token'] || req.query.token || req.body.token;
+  if (!token) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+  const requester_id = Tokenizer.userIdFromToken(token);
+  console.log(requester_id);
+  if (!requester_id) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+  const { id } = req.query;
+  if (!id) {
+    return res.status(400).json({
+      message: 'Please provide an id',
+    });
+  }
+  User.findById(id, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Error getting user',
+      });
+    }
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      });
+    }
+    if (user.followers.indexOf(requester_id) === -1) {
+      return res.status(401).json({
+        message: "Not following, you're not authorized to see this user's followers",
+      });
+    }
+    User.find({ _id: { $in: user.followers } }, (err, users) => {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error getting followers',
+        });
+      }
+      return res.status(200).json({
+        message: 'Followers found',
+        followers: users.map((user) => ({
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          birthdate: user.birthdate,
+          bio: user.bio,
+          follow_count: user.follows.length,
+          follower_count: user.followers.length,
+          post_count: user.posts.length,
+          liked_post_count: user.liked_posts.length,
+        })),
+      });
+    });
+  });
+};
+
+const getFollows = async (req, res) => {
+  const token = req.headers['x-access-token'] || req.query.token || req.body.token;
+  if (!token) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+  const requester_id = Tokenizer.userIdFromToken(token);
+  if (!requester_id) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+  const { id } = req.query;
+  if (!id) {
+    return res.status(400).json({
+      message: 'Please provide an id',
+    });
+  }
+  User.findById(id, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Error getting user',
+      });
+    }
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      });
+    }
+    if (user.followers.indexOf(requester_id) === -1 && id !== requester_id) {
+      return res.status(401).json({
+        message: "Not following, you're not authorized to see this user's follows",
+      });
+    }
+    User.find({ _id: { $in: user.follows } }, (err, users) => {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error getting follows',
+        });
+      }
+      return res.status(200).json({
+        message: 'Follows found',
+        follows: users.map((user) => ({
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          birthdate: user.birthdate,
+          bio: user.bio,
+          follow_count: user.follows.length,
+          follower_count: user.followers.length,
+          post_count: user.posts.length,
+          liked_post_count: user.liked_posts.length,
+        })),
+      });
+    });
+  });
+};
+
+module.exports = { registerUser, getUser, loginUser, getFollowers, getFollows };
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const Tokenizer = require('../utils/token.util');
 const router = express.Router();
 const controller = require('../controllers/users.controller');
 
