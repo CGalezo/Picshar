85af0a115ddb44b8eb1d972475e5e4b5cbcb9f08
Guacha <guachamalinga@gmail.com>
2022-06-01 14:21:36 -0500

    feat: added controller function to accept/reject followRequests

--- backend/controllers/followRequest.controller.js
+++ backend/controllers/followRequest.controller.js
@@ -46,6 +46,86 @@ const createFollowRequest = async (req, res) => {
   });
 };
 
+const handleFollowRequest = async (req, res) => {
+  const token = req.headers['x-access-token'] || req.query.token || req.body.token;
+  if (!token) {
+    return res.status(401).json({
+      message: 'Please provide a token',
+    });
+  }
+  const requestee_id = Tokenizer.userIdFromToken(token);
+  if (!requestee_id) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+  const { request_id, action } = req.body;
+  if (!request_id) {
+    return res.status(401).json({
+      message: 'Please provide a request_id',
+    });
+  }
+  if (!action) {
+    return res.status(401).json({
+      message: 'Please provide an action',
+    });
+  }
+  if (action !== 'accept' && action !== 'reject') {
+    return res.status(401).json({
+      message: 'Please provide a valid action',
+    });
+  }
+  FollowRequest.findById(request_id, (err, request) => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Error finding follow request',
+      });
+    }
+    if (!request) {
+      return res.status(404).json({
+        message: 'Follow request not found',
+      });
+    }
+    if (request.requestee.toString() !== requestee_id) {
+      return res.status(401).json({
+        message: 'You are not authorized to accept or reject this follow request',
+      });
+    }
+    if (action === 'accept') {
+      User.findById(request.requestee, (err, user) => {
+        if (err) {
+          return res.status(500).json({
+            message: 'Error finding user',
+          });
+        }
+        if (!user) {
+          return res.status(404).json({
+            message: 'Requestee user not found',
+          });
+        }
+        user.followers.push(request.requester);
+        user.save((err, user) => {
+          if (err) {
+            return res.status(500).json({
+              message: 'Error saving user',
+            });
+          }
+          request.remove();
+          return res.status(200).json({
+            message: 'Follow request accepted',
+          });
+        });
+      });
+    } else {
+      request.remove();
+      return res.status(200).json({
+        message: 'Follow request rejected',
+      });
+    }
+  });
+};
+
 module.exports = {
   createFollowRequest,
+  handleFollowRequest,
 };
