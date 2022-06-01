66d09be22e43819f74ac39a939316da2fa12f317
Guacha <guachamalinga@gmail.com>
2022-06-01 17:12:15 -0500

    test: registration testing (whole data)

--- backend/tests/users.test.js
+++ backend/tests/users.test.js
@@ -47,6 +47,44 @@ describe('User Login Testing', () => {
     expect(response.status).toBe(401);
     expect(response.body.message).toBe('Invalid password');
   });
+
+  it('Should login properly when supplied a valid token', async () => {
+    const response = await request(app).post('/users/login').send({
+      username: 'testUser',
+      password: 'testPassword',
+    });
+    const token = response.body.token;
+    const response2 = await request(app).post('/users/login').set('x-access-token', token);
+    expect(response2.status).toBe(200);
+    expect(response2.body.token).toBeDefined();
+  });
+});
+
+describe('User Registration testing', () => {
+  it('Should properly register a user with valid info', async () => {
+    const response = await request(app).post('/users/').send({
+      username: 'testUser2',
+      password: 'testPassword',
+      email: 'email2@mail.com',
+      birthdate: new Date(),
+      bio: 'test bio 2',
+    });
+    expect(response.status).toBe(201);
+    expect(response.body.message).toBe('User created');
+    expect(response.body.token).toBeDefined();
+  });
+
+  it('Should fail gracefully when username is already taken', async () => {
+    const response = await request(app).post('/users/').send({
+      username: 'testUser2',
+      password: 'testPassword',
+      email: 'anothermail@mail.com',
+      birthdate: new Date(),
+      bio: 'test bio 2',
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('User already exists');
+  });
 });
 
 afterAll(async () => {
